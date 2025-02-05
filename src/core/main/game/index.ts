15aa113c8ad972a978390beda02d281cb080d2ea
blhsrwznrghfzpr <blhsrwznrghfzpr.hompyhrln@gmail.com>
2025-02-05 21:07:53 +0900

    render workeroid traverser

--- src/core/main/game/index.ts
+++ src/core/main/game/index.ts
@@ -1,12 +1,14 @@
-import { Hex } from "honeycomb-grid";
+import { HexCoordinates, line } from "honeycomb-grid";
 import { GameState } from "./type";
 import { Workeroid } from "./workeroid";
 import { StageMap } from "./map";
+import { Cell } from "./map/cell";
 
 export class Game {
   gameState: GameState;
 
   constructor() {
+    console.debug("Game created");
     this.gameState = {
       mode: "lobby",
       players: [],
@@ -36,13 +38,42 @@ export class Game {
       return;
     }
 
-    const baseHex = new Hex([0, 0]);
+    const baseCell = new Cell([0, 0]);
 
     this.gameState = {
       mode: "inGame",
       players: this.gameState.players,
-      workers: Array.from({ length: 10 }).map(() => new Workeroid(baseHex)),
+      workers: Array.from({ length: 10 }).map(() => new Workeroid(baseCell)),
       map: new StageMap({ radius: 10 }),
     };
+
+    this.moveWorker(0, [5, 3]);
+  }
+
+  resetGame() {
+    this.gameState = {
+      mode: "lobby",
+      players: [],
+    };
+  }
+
+  moveWorker(workerId: number, dst: HexCoordinates) {
+    if (this.gameState.mode !== "inGame") {
+      return;
+    }
+
+    const worker = this.gameState.workers[workerId];
+    const dstCell = this.gameState.map.grid.getHex(dst);
+
+    if (!worker || !dstCell) {
+      return;
+    }
+
+    const currentCell = worker.status.currentCell;
+    const traverser = this.gameState.map.grid.traverse(
+      line({ start: currentCell, stop: dstCell }),
+    );
+
+    worker.move(traverser.toArray());
   }
 }
--- src/core/main/game/workeroid/index.ts
+++ src/core/main/game/workeroid/index.ts
@@ -1,10 +1,49 @@
-import { Hex } from "honeycomb-grid";
+import { Cell } from "../map/cell";
+
+export type WorkerStatus = WorkerIdle | WorkerMoving;
+
+type WorkerIdle = {
+  type: "idle";
+  currentCell: Cell;
+};
+
+type WorkerMoving = {
+  type: "moving";
+  currentCell: Cell;
+  traverser: Cell[];
+};
 
 export class Workeroid {
-  currentHex: Hex;
-  // ability: WorkerAbility;
+  status: WorkerStatus;
 
-  constructor(currentHex: Hex) {
-    this.currentHex = currentHex;
+  constructor(currentCell: Cell) {
+    this.status = { type: "idle", currentCell: currentCell };
+  }
+
+  move(traverser: Cell[]) {
+    this.status = {
+      type: "moving",
+      currentCell: this.status.currentCell,
+      traverser,
+    };
+
+    const timer = setInterval(() => {
+      console.log("Workeroid move timer", this.status.currentCell.tuple);
+      if (this.status.type !== "moving" || this.status.traverser.length === 0) {
+        this.status = {
+          type: "idle",
+          currentCell: this.status.currentCell,
+        };
+        clearInterval(timer);
+        return;
+      }
+
+      const nextCell = this.status.traverser.shift();
+      if (!nextCell) {
+        return;
+      }
+
+      this.status.currentCell = nextCell;
+    }, 1000);
   }
 }
